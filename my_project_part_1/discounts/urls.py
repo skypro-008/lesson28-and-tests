ef97630bf2d26a7298c6b8bbbd23bed610aef697
lisvv <81390205+lisvv@users.noreply.github.com>
2022-02-15 18:28:05 +0300

    urls

--- my_project_part_1/discounts/urls.py
+++ my_project_part_1/discounts/urls.py
@@ -1,8 +1,4 @@
 # TODO настраиваем urls здесь
-from discounts.views import DiscountDetailView, DiscountListView
-from django.urls import path
-
 urlpatterns = [
-    path("discount/", DiscountListView.as_view()),
-    path("discount/<int:pk>/", DiscountDetailView.as_view()),
+
 ]
--- solution/my_project_part_1/discounts/urls.py
+++ solution/my_project_part_1/discounts/urls.py
@@ -1,4 +1,8 @@
-# TODO настраиваем urls здесь
-urlpatterns = [
+from django.urls import path
 
+from discounts.views import DiscountDetailView, DiscountListView
+
+urlpatterns = [
+    path("discount/", DiscountListView.as_view()),
+    path("discount/<int:pk>", DiscountDetailView.as_view()),
 ]
